6f698b399239e9fd5263006289db77e9777d04d9
Dinesh Sharma <dineshsharmadev93@gmail.com>
2024-06-11 09:16:17 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,60 @@
-import { useEffect, useState } from "react"
-import Highlights from "./components/Highlights"
+import { useEffect, useState } from "react";
+import Highlights from "./components/Highlights";
 import Temperature from "./components/Temperature";
 
-import axios from "axios"
+import axios from "axios";
 import Timer from "./components/Timer";
 function App() {
-    const [weatherdata,setWeatherData] = useState([])
-    const [city,setCity] = useState("Jaipur")
-    // const getapi = () => {
-    //   const api = `http://api.weatherapi.com/v1/current.json?key=86765e6ed1254bf3afa132309240202&q=${city}&aqi=no`
-    //      fetch(api)
-    //     .then((response)=>{
-    //       if (!response.ok) {
-    //         throw new Error("Could not get data");
-    //       }
-    //         return response.json()
-    //     })
-    //     .then((data)=>{
-    //       setWeatherData([data])
-    //     }).catch((e)=>{
-    //       console.log(`error ${e}`);
-    //     })
-    // }
-    const getapi =async()=>{
-      const api = `http://api.weatherapi.com/v1/current.json?key=86765e6ed1254bf3afa132309240202&q=${city}&aqi=no`
-       try {
-          const result = await axios.get(api)
-           setWeatherData([result.data])
-          
-       } catch (error) {
-         console.error(error);
-       }
+  const [weatherdata, setWeatherData] = useState([]);
+  const [city, setCity] = useState("Jaipur");
+  // const getapi = () => {
+  //   const api = `http://api.weatherapi.com/v1/current.json?key=86765e6ed1254bf3afa132309240202&q=${city}&aqi=no`
+  //      fetch(api)
+  //     .then((response)=>{
+  //       if (!response.ok) {
+  //         throw new Error("Could not get data");
+  //       }
+  //         return response.json()
+  //     })
+  //     .then((data)=>{
+  //       setWeatherData([data])
+  //     }).catch((e)=>{
+  //       console.log(`error ${e}`);
+  //     })
+  // }
+  const getapi = async () => {
+    const api = `http://api.weatherapi.com/v1/current.json?key=86765e6ed1254bf3afa132309240202&q=${city}&aqi=no`;
+    try {
+      const result = await axios.get(api);
+      setWeatherData([result.data]);
+    } catch (error) {
+      console.error(error);
     }
-    console.log(weatherdata);
-      useEffect(()=>{
-        getapi()
-      },[city])
-   
+  };
+  console.log(weatherdata);
+  useEffect(() => {
+    getapi();
+  }, [city]);
+
   return (
-     <div className="bg-gray-900 h-screen flex justify-center align-top">
-      
-         <div className=" w-1/4 h-96 mt-28">
-          
-          { 
-          weatherdata &&
-            weatherdata.map((item,index)=>(
-          <Temperature  item={item}key={index} setCity={setCity} />
-              
-            ))
-          }
-         </div>
-         <div className=" w-1/3  h-96 mt-36 ml-3">
-          {
-            weatherdata.length >0 ?weatherdata.map((item,index)=>(
-             <Highlights data={item} key={index}/>
-               
-            ))
-            : <span className="text-white text-4xl">Loadinng...</span>
-          }
-         </div>
-         
-     </div>
-  )
+    <div className="bg-gray-900 h-screen flex justify-center align-top gap-20">
+      <div className=" w-1/4 h-96 mt-28">
+        {weatherdata &&
+          weatherdata.map((item, index) => (
+            <Temperature item={item} key={index} setCity={setCity} />
+          ))}
+      </div>
+      <div className=" w-1/3  h-96 mt-36 ml-3">
+        {weatherdata.length > 0 ? (
+          weatherdata.map((item, index) => (
+            <Highlights data={item} key={index} />
+          ))
+        ) : (
+          <span className="text-white text-4xl">Loadinng...</span>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { SlLocationPin } from "react-icons/sl";
 
 export default function Temperature({ item, setCity }) {
+  // get the input value through states
+  const [inpval,setInpVal] = useState("")
   function handlechange(e) {
-    setCity(e.target.value);
+    setInpVal(e.target.value);
+  }
+  // submit button when it triggerd that time data will fetch through api
+  const submit = () =>{
+       setCity(inpval)
   }
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <div className="flex justify-center ">
         <input
           type="text"
-          className="bg-slate-600 p-2 rounded-sm focus:outline-none text-slate-400 text-center"
+          className="bg-slate-600 p-2 rounded-sm focus:outline-none text-slate-400 text-center font-semibold tracking-widest"
           placeholder="Enter your city name"
-          defaultValue={"New Delhi"}
+          defaultValue={"Jaipur"}
           onChange={handlechange}
         />
-        <span className="text-white ml-4 mt-2">
-          <SlLocationPin className="text-2xl text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out" />
+        <span className="text-white ml-4 mt-1">
+          {/* <SlLocationPin className="text-2xl text-transform scale-100 hover:scale-110 transition-transform duration-300 ease-in-out" /> */}
+          <button onClick={submit} className="border w-28 h-8 rounded-sm font-semibold tracking-widest bg-slate-600 text-slate-400 hover:bg-slate-800 hover:text-white">Submit</button>
         </span>
       </div>
       <div className="flex justify-center mt-3s">
@@ -33,8 +40,8 @@ export default function Temperature({ item, setCity }) {
         <span className="text-xl">{item.current.condition.text}</span>
       </div>
       <div className="flex justify-center mt-8 text-white">
-        <span className="text-slate-500 ">
-          Today / {item.location.localtime} / {item.location.name},{item.location.region},({item.location.country})
+        <span className="text-slate-300 text-[16px] italic">
+          Today /{item.location.localtime}/{item.location.name},{item.location.region},({item.location.country})
         </span>
       </div>
     </div>
